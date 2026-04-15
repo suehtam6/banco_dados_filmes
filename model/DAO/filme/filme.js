18b87542b046c8b301af1e1986affa558f0298df
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-04-15 16:29:48 -0300

    Fazendo um banco de dados sobre filmes

---
+++ model/DAO/filme/filme.js
@@ -0,0 +1,36 @@
+/*************************************************************************************
+ * Objetivo: Arquivo responsável pelo CRUD de dados do Filme banco de dados mySQL
+ * Data: 2026/04/15
+ * Autor: Matheus Lucas
+ * Versão: 1.0
+ *************************************************************************************/
+
+
+// Função para inserir um novo filme no banco de dados
+const insertFilme = async function(filme){
+}
+
+// Função para atualizar um filme existente no banco de dados
+const updateFilme = async function(filme){
+}
+
+// Função para retornar todos os dados de filmes do banco de dados
+const selectAllFilme = async function(){
+}
+
+// Função para selecionar um filme através do ID do filme
+const selectByIdFilme = async function(id){
+}
+
+// Função para deletar um filme existente no banco de dados através do ID
+const deleteFilme = async function(id){
+}
+
+module.exports = {
+    insertFilme,
+    updateFilme,
+    selectAllFilme,
+    selectByIdFilme,
+    deleteFilme
+}
+
---
+++ model/database/scriptSQL_filmes.sql
@@ -0,0 +1,50 @@
+#Permite criar um database
+create database db_filmes_20261_b;
+
+#Permite visualizar todos os databases existentes
+show databases;
+
+#Permite escolher o database a ser utilizado
+use db_filmes_20261_b;
+
+#Permite visualizar todas as tabelas existentes dentro do database
+show tables;
+
+#Permite criar tabelas
+create table tbl_filme (
+	id					int not null auto_increment primary key,
+    nome 				varchar(80) not null,
+    sinopse 			text not null,
+    capa 				varchar(255) not null,
+    data_lancamento 	date not null,
+    duracao 			time not null,
+    valor 				decimal(5,2) default 0,
+    avaliacao 			decimal(3,2) default null
+);
+
+# Permite apagar qualquer tabela e até mesmo o database, !!não deve ser utilizado em nenhum momento caso tenho dados na tabela e database!!
+#drop table tbl_filme
+#drop database db_filmes_20261_b
+
+# Permite inserir itens na tabela, mas deve sempre ser seguido na ordem para evitar que inverta algum item
+insert into tbl_filme(
+	nome,
+	sinopse,
+	capa,
+	data_lancamento,
+	duracao,
+	valor,
+	avaliacao
+
+)values (
+	"Super Mario Galaxy: O Filme",
+    "Uma nova aventura leva Mario a enfrentar um inédito e ameaçador super vilão. Em Super Mario Galaxy: O Filme,
+    o bigodudo encanador italiano e seus aliados embarcam numa aventura galáctica repleta de ação e 
+    momentos emocionantes depois de salvar o Reino dos Cogumelos.",
+    "https://br.web.img3.acsta.net/c_310_420/img/5b/ea/5bea1aeac3323aeaaf82449a34fafbbf.jpg",
+    "2026-04-02",
+    
+    
+)
+
+
