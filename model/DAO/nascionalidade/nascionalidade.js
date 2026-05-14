a01a15532a41717bd75a9a752acf038415a72fbe
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-05-14 15:00:03 -0300

    Começando a fazer a tabela nascionalidade

---
+++ model/DAO/nascionalidade/nascionalidade.js
@@ -0,0 +1,32 @@
+/***************************************************************************************************************
+ * Objetivo: Arquivo responsável pelo CRUD de dados da nascionalidade dos profissionais banco de dados mySQL
+ * Data: 2026/05/14
+ * Autor: Matheus Lucas
+ * Versão: 1.0
+ ***************************************************************************************************************/
+
+// Import da biblioteca Knex para manipular dados no Banco de dados MySQL.
+const knex = require('knex')
+
+// Import do arquivo para configuração para acesso ao banco de dados.
+const knexDataBaseConfig = require('../../database_config/knexConfig.js')
+
+const insertNascionalidade = async function(nascionalidade) {
+    
+}
+
+const updateNascionalidade = async function(nascionalidade) {
+    
+}
+
+const selectAllNascionalidade = async function() {
+    
+}
+
+const selectByIdNascionalidade = async function(id) {
+    
+}
+
+const deleteNascionalidade = async function(id){
+    
+}
